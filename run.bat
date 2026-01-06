5cc6684d74936846aacd06468e087c870f875f33
Anuj <anujkumar2662@gmail.com>
2026-01-06 12:16:50 +0530

    home page html done for desktop view

--- run.bat
+++ run.bat
@@ -1,1 +1,13 @@
-npx tailwindcss -i ./src/style.css -o ./dist/style.css --watch
+@echo off
+echo ============================
+echo Tailwind + SCSS Compiler
+echo ============================
+
+:: Tailwind watching
+start cmd /k "npx tailwindcss -i ./src/tailwind/tailwind-input.css -o ./css/tailwind.css --watch"
+
+:: SCSS watching
+start cmd /k "npx sass ./src/scss/main.scss ./css/main.css --style=compressed --watch"
+
+echo Watching for changes...
+pause
